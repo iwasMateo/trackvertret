196f4ceb8958aebfbd51fbda70b65059db9313a4
iwasMateo <iwasmateo@gmail.com>
2026-02-26 20:51:26 +0100

    added month handler

--- Vertretungsplan.js
+++ Vertretungsplan.js
@@ -21,18 +21,23 @@ let day = date.getDate();
 //}
 function doshit() {
   const div = document.getElementById("container");
-  for (i=0; i<3; i++) {
+  for (i=0; i<5; i++) {
     console.log(i);
-    let currentURL = `https://ceciliengymnasium.de/attachments/article/1512/Vertretungsplan_${day+i}.${month}.20${year}.pdf`;
     //console.log(currentURL);
     //https://ceciliengymnasium.de/attachments/article/1372/Vertretungsplan_26.01.2026.pdf
     //const proxyUrl = `https://api.allorigins.win{encodeURIComponent(currentURL)}`;
     //if (await linkGood(pro)) {
     console.log(month);
-    let weekday = new Date((date.getFullYear()%100)+2000, date.getMonth(), day+i)
+    let weekday = new Date((date.getFullYear()%100)+2000, date.getMonth(), day)
+    weekday.setDate(weekday.getDate()+i)
     console.log(weekday);
+    let tempmonth = weekday.getMonth()
+    if (tempmonth < 10) {
+      tempmonth = `0${tempmonth+1}`
+    }
+    let currentURL = `https://ceciliengymnasium.de/attachments/article/1512/Vertretungsplan_${weekday.getDate()}.${tempmonth}.20${weekday.getFullYear%100}.pdf`;
     if (weekday.getDay() <6) {
-      container.insertAdjacentHTML('beforeend', `<div><p><a href='${currentURL}'>Vertretungsplan ${day+i}-${month}-${year}</a></p></div><br>`);
+      container.insertAdjacentHTML('beforeend', `<div><p><a href='${currentURL}'>Vertretungsplan ${weekday.getDate()}-${month}-${year}</a></p></div><br>`);
     } else {
       console.log("The day was "+weekday.getDay());
     }
